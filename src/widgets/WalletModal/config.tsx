f7598755b70b9f7748b190ba3fda765a88edc870
WombatFromHell <WombatFromHell@users.noreply.github.com>
2021-12-18 13:58:19 -0700

    Enable WalletConnect support

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,13 +1,21 @@
 import Metamask from "./icons/Metamask";
+import WalletConnect from "./icons/WalletConnect";
 // import Coin98Wallet from "./icons/Coin98Wallet";
 // import Coinbase from "./icons/Coinbase";
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
   {
-    title: "MetaMask",
+    title: "Metamask",
     icon: Metamask,
     connectorId: ConnectorNames.Injected,
+    priority: 1,
+  },
+  {
+    title: "WalletConnect",
+    icon: WalletConnect,
+    connectorId: ConnectorNames.WalletConnect,
+    priority: 2,
   },
   // {
   //   title: "Coin98 Wallet",
